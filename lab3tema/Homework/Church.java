f238122aa61a7ed68a4f0cf01bac35ef5993ce34
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-03-18 20:42:03 +0200

    Update Church.java

--- lab3tema/Homework/Church.java
+++ lab3tema/Homework/Church.java
@@ -1,20 +1,34 @@
-package Homework;
 import java.time.LocalTime;
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * Represents a church attraction.
+ */
 public abstract class Church extends Attraction {
 
+    /**
+     * Constructs a new Church object with the given name.
+     * @param name The name of the church.
+     */
     public Church(String name) {
         super(name);
     }
 
+    /**
+     * Checks if the church is visitable.
+     * @return True, indicating the church is always visitable.
+     */
     @Override
     public boolean isVisitable() {
         return true; // Church is always visitable
     }
 
+    /**
+     * Retrieves the visiting timetable for the church.
+     * @return A map containing the visiting timetable, mapping dates to time intervals.
+     */
     @Override
     public Map<LocalDate, TimeInterval<LocalTime>> getVisitingTimetable() {
         Map<LocalDate, TimeInterval<LocalTime>> visitingTimetable = new HashMap<>();
