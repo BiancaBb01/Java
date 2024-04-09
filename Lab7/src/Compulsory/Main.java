3c5c194f731c60cbb34cd731e3afa55ee41458f0
Bianca Bărăscu <126165357+BiancaBb01@users.noreply.github.com>
2024-04-09 20:00:32 +0300

    Add files via upload

---
+++ Lab7/src/Compulsory/Main.java
@@ -0,0 +1,14 @@
+package Compulsory;
+
+public class Main {
+    public static void main(String[] args) {
+        int numPlayers = 4;
+        int n = 3;
+        TokenBag bag = new TokenBag(n);
+
+        for (int i = 0; i < numPlayers; i++) {
+            Player player = new Player("Player " + (i + 1), bag);
+            new Thread(player).start();
+        }
+    }
+}
